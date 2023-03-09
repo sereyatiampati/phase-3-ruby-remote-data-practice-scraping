77923823ea8f398bbd442dbf94f6aa4c016ec9a0
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-09 15:37:53 +0300

    scrapping is fun

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = URI.open("https://flatironschool.com/")
+
+# doc=Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+courses = doc.css(".heading-40-semibold")
+
+# data = courses.map do |course|
+#   course.text.strip
+# end
+# pp data
+p courses[0].children
+
+
